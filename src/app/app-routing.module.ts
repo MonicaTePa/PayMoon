a1ec648a57ffa0db0f4844613437000d272c50bb
mariamqco <mariamqco@gmail.com>
2022-03-13 19:30:37 -0500

    user-desktop (cuenta) version telefono

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserDesktopComponent } from './components/user-desktop/user-desktop.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'user-desktop', component: UserDesktopComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardsComponent } from './components/cards/cards.component';
+import { UserDesktopComponent } from './components/user-desktop/user-desktop.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { UserNavbarComponent } from './components/user-navbar/user-navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardsComponent
+    CardsComponent,
+    UserDesktopComponent,
+    UserProfileComponent,    
+    UserNavbarComponent
   ],
   imports: [
     BrowserModule,
