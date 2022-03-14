e24c5ff070769f29330f4d705f11446cdf8977b7
mariamqco <mariamqco@gmail.com>
2022-03-14 14:50:48 -0500

    componente user-profile ruta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserDesktopComponent } from './components/user-desktop/user-desktop.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
 
 const routes: Routes = [
-  {path: 'user-desktop', component: UserDesktopComponent}
+  {path: 'user-desktop', component: UserDesktopComponent},
+  {path: 'user-profile', component: UserProfileComponent}
 ];
 
 @NgModule({
