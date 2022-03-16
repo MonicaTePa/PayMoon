94f4b6708dd57fe76f42e5c87e90e91150308c0a
Adriana Sandoval <adrianaaasr@gmail.com>
2022-03-15 20:32:48 -0500

    commit Adriana

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardsComponent } from './components/cards/cards.component';
+import { HomeComponent } from './components/home/home.component';
+import { ErrorComponent } from './components/error/error.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardsComponent
+    CardsComponent,
+    HomeComponent,
+    ErrorComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
