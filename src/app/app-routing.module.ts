7674da1ae53d1f795f5dcb4cdc71304d9a247fdd
MonicaTePa <moniktepamatp@gmail.com>
2022-03-15 21:27:12 -0500

    changes in cards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserDesktopComponent } from './components/user-desktop/user-desktop.component';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
-
+import { Page404Component } from './components/page404/page404.component';
+import { HomeComponent } from './components/home/home.component';
 const routes: Routes = [
+  {path:'',component:HomeComponent},
   {path: 'user-desktop', component: UserDesktopComponent},
-  {path: 'user-profile', component: UserProfileComponent}
+  {path: 'user-profile', component: UserProfileComponent},
+  {path:'**',component:Page404Component},
 ];
 
 @NgModule({
