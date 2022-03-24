2aebc832e24f2ef464362ba0dd2fe195d88fa7b7
rihgas <rihgas360@gmail.com>
2022-03-23 21:33:00 -0500

    fixed bug in routerlink

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder,FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
