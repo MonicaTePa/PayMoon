851c81ce68f5f2dd3127c19ac8d9aa5f541c628c
mariamqco <mariamqco@gmail.com>
2022-03-26 19:12:16 -0500

    Implementación de pago y recepción de dinero (transactions)

--- src/app/components/transfers/transfers.component.ts
+++ src/app/components/transfers/transfers.component.ts
@@ -5,6 +5,8 @@ import { TransactionService } from 'src/app/services/transaction.service';
 import { UserService } from 'src/app/services/user.service';
 import { Card } from 'src/app/models/card.model';
 import { CardService } from 'src/app/services/card.service';
+import { Pocket } from 'src/app/models/pocket.model';
+import { PocketService } from 'src/app/services/pocket.service';
 
 @Component({
   selector: 'app-transfers',
@@ -22,11 +24,13 @@ export class TransfersComponent implements OnInit {
   pocket_former_balence: number = 0;
   latest_deposite: number = 0;
 
+  receiver_pocket: Pocket | null = null
+
   transferRegisterForm: FormGroup;
 
   numbersRegex = /^[0-9]+$/;  
   
-  constructor(private fb: FormBuilder, private user_service: UserService, private transaction_service: TransactionService, private card_service: CardService) { 
+  constructor(private fb: FormBuilder, private user_service: UserService, private transaction_service: TransactionService, private card_service: CardService, private pocket_service: PocketService) { 
     this.transferRegisterForm = this.fb.group({
       id_card: [[Validators.required]],
       receiver_phone: ['',[Validators.required, Validators.pattern(this.numbersRegex)]],
@@ -51,16 +55,15 @@ export class TransfersComponent implements OnInit {
 
   makeTransfer(){
     const phone_number = this.transferRegisterForm.get('receiver_phone')?.value
-    const transfer_amount = this.transferRegisterForm.get('amount')?.value
-    const id_card = this.transferRegisterForm.get('id_card')?.value
-
-    
+    const transfer_amount = parseInt(this.transferRegisterForm.get('amount')?.value);
+    const id_card = this.transferRegisterForm.get('id_card')?.value       
 
     this.user_service.getUserByPhone(phone_number).subscribe(
       data =>{
         if(data.message){
           console.log(data.message);
         }else{
+          
           console.log(data);
           const id_receiver = data;          
           const transaction : Transaction = {            
@@ -69,24 +72,103 @@ export class TransfersComponent implements OnInit {
             amount: transfer_amount,
             type: id_card == "none"?"Bolsillo":"Tarjeta",
             id_card: id_card == "none"?"No aplica":id_card
-          }
-          this.transaction_service.postTransaction(transaction).subscribe(
+          } 
+
+          this.pocket_service.getPocketById(transaction.id_receiver).subscribe(
             data =>{
-              console.log(data.message);
-            },error =>{
-              console.log("Hubo un error");
+              if(!data.message){
+                let receiver_pocket = data;
+                console.log(receiver_pocket);
+                this.pocket_service.getPocketById(transaction.id_payer).subscribe(
+                  data=>{
+                    if(!data.message){
+                      let payer_pocket = data;
+                      console.log(payer_pocket);
+                      console.log(receiver_pocket);
+
+                      receiver_pocket.former_balance = receiver_pocket.balance;
+                      receiver_pocket.balance = receiver_pocket.balance + transaction.amount;
+                      receiver_pocket.receptions = receiver_pocket.receptions+ transaction.amount
+
+                      payer_pocket.former_balance = payer_pocket.balance;
+                      payer_pocket.balance = transaction.id_card=="none"?payer_pocket.balance:payer_pocket.balance - transaction.amount;
+                      payer_pocket.payments = payer_pocket.payments + transaction.amount;
+
+                      console.log(payer_pocket);
+                      console.log(receiver_pocket);
+
+                      this.pocket_service.putPocket(receiver_pocket._id,receiver_pocket).subscribe(
+                        data =>{ 
+                          if(!data.message){
+                            console.log(data);
+                            this.pocket_service.putPocket(payer_pocket._id,payer_pocket).subscribe(
+                              data =>{
+                                if(data.messsage){
+                                  console.log(data.message);
+                                }else{
+                                  console.log(data);
+                                  console.log("Pago realizado");
+
+                                  this.transaction_service.postTransaction(transaction).subscribe(
+                                    data =>{
+                                      console.log(data.message);
+                                      
+                                    },error =>{
+                                      console.log("Hubo un error");
+                                      console.log(error);
+                                    }
+                                  );
+                                }
+                              }
+                              , error =>{
+                                console.log("Hubo un error");
+                                console.log(error)
+                              }
+                            );
+                          }else(
+                            console.log(data.message)
+                          )
+                        },error =>{
+                          console.log("Hubo un error");
+                          console.log(error)
+                        }
+                      );
+
+                      
+
+                      
+                    }else{
+                      console.log(data)
+                    }
+                  },error=>{
+                    console.log(error);
+                  }
+                );
+              }else{
+                console.log(data);
+              }
+              
+              
+            }, error =>{
               console.log(error);
             }
           );
-        }
-      },error =>{
-        console.log(error);
-        console.log("Hubo un error");
-      }
-    );
+          
+          
 
-   
-  
+          
+         
+        }
+      },error =>{       
+        console.log("Hubo un error");
+        console.log(error);
+      }
+    );         
   }
+
+ 
+
+  
+  
 
 }
--- src/app/models/pocket.model.ts
+++ src/app/models/pocket.model.ts
@@ -4,14 +4,18 @@ export class Pocket{
     balance?: number;
     receptions?: number;
     payments?: number;
-    deposits?: number;    
+    deposits?: number;
+    former_balance?: number;  
+    last_deposit?: number;  
 
-    constructor(id_user: string, balance?: number, receptions?:number, payments?:number,deposits?:number){
+    constructor(id_user: string, balance?: number, receptions?:number, payments?:number,deposits?:number, former_balance?: number, last_deposit?: number){
         this.id_user = id_user;
         this.balance = balance;
         this.receptions = receptions;
         this.payments = payments;
         this.deposits = deposits;
+        this.former_balance = former_balance? former_balance: 0;
+        this.last_deposit = last_deposit? last_deposit: 0;        
     }
 }
 
